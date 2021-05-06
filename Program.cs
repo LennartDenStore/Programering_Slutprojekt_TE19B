bb4265daf5a559a77062570c32a5e52b53d7f26f
LennartDenStore <70271050+LennartDenStore@users.noreply.github.com>
2021-05-06 11:01:41 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Faling_rocks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+// Start meny (cases)
+
+// Start game
+
+// Loade obekts and map
+
+// movment (if loop)
+
+// if player hit by objekt open menue to restart or exit game loop
+
+//if player isent hit by objekt loop movment and objekts untill loop ends
+
+// if player preses escape open menue (return to game, restart or exit)
+
+// if exit end game
+        }
+    }
+}
